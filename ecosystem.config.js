7c23d6bc1a45fb69f6e5e12bc0630430212bccb7
Nelson Gallego <nelson@remotepcsolutions.com>
2021-12-10 03:56:23 -0500

    update test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,26 @@
 module.exports = {
-  apps : [{
+  apps: [{
     name: 'APIREST',
     script: 'npm start',
     watch: 'true',
-        env_production: {
-            'PORT': '3000',
-            'MONGODB_HOST': 'ApiDelilahRestoNel',
-            'MONGODB_PASS': 'Gt2wVhLrs7WC5u6J',
-            'MONGODB_NAME_DATABASE': 'apidelilah',
-            'SALT_BCRYPT': 10,
-            'PASS': 'ANDLIa7dsyoq8wgdblashdO7ADYO87QdhiQDO892qeHQOñ',
-        },
-}]
+    env_production: {
+      'PORT': '3000',
+      'MONGODB_HOST': 'ApiDelilahRestoNel',
+      'MONGODB_PASS': 'Gt2wVhLrs7WC5u6J',
+      'MONGODB_NAME_DATABASE': 'apidelilah',
+      'SALT_BCRYPT': 10,
+      'PASS': 'ANDLIa7dsyoq8wgdblashdO7ADYO87QdhiQDO892qeHQOñ',
+    },
+  }, {
+    name: 'API-TEST',
+    script: 'npm test',
+    watch: 'true',
+    env_test: {
+      'MONGODB_HOST': 'ApiDelilahRestoNel',
+      'MONGODB_PASS': 'Gt2wVhLrs7WC5u6J',
+      'MONGODB_NAME_DATABASE': 'apidelilah',
+      'SALT_BCRYPT': 10,
+      'PASS': 'ANDLIa7dsyoq8wgdblashdO7ADYO87QdhiQDO892qeHQOñ',
+    }
+  }]
 };
